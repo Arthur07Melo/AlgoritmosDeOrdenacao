460d30711ca35a65791d3d1fe76d1932faef179e
Arthur07Melo <aarthur00melo@gmail.com>
2023-09-09 15:16:47 -0300

    Algoritmos de ordenação finalizados. Restando: Bubble e Insertion

---
+++ src/atividade02/App.java
@@ -0,0 +1,131 @@
+package atividade02;
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        calcular_media(15000, 50);
+    }
+
+
+    public static void calcular_media(int tamVetor, int iter){
+        Ordenacao orden = new Ordenacao();
+        int arrayCrescente[] = cria_arrayAleatorio(tamVetor);
+        int arrayDecrescente[] = cria_arrayDecrescente(tamVetor);
+        int arrayAleatorio[] = cria_arrayAleatorio(tamVetor); 
+
+        long mergeTime = 0;
+        long quickTime = 0;
+        long selectionTime = 0;
+        long insertionTime = 0;
+        long countingTime = 0;
+        long bubbleTime = 0;
+        long random_quickTime = 0;
+        long java_quickTime = 0;
+
+        for(int i = 0; i<=iter; i++){
+            mergeTime += orden.mergeSort(arrayCrescente.clone());
+            quickTime += orden.quickSort(arrayCrescente.clone());
+            random_quickTime += orden.random_quickSort(arrayCrescente.clone());
+            java_quickTime += orden.quickSort_Java(arrayCrescente.clone());
+            selectionTime += orden.selectionSort(arrayCrescente.clone());
+            countingTime += orden.countingSort(arrayCrescente.clone());
+            insertionTime += orden.insertionSort(arrayCrescente.clone());
+            bubbleTime += orden.bubbleSort(arrayCrescente.clone());
+        }
+        
+        System.out.printf("""
+                bubbleSort time:      %d
+                insertionSort time:   %d
+                selectionSort time:   %d
+                mergeSort time:       %d
+                quickSort time:       %d
+                randomQuickSort time: %d
+                javaQuickSort time:   %d
+                countingSort time:    %d
+                """, bubbleTime, insertionTime, selectionTime, mergeTime, quickTime, random_quickTime, java_quickTime, countingTime);
+
+         mergeTime = 0;
+         quickTime = 0;
+         selectionTime = 0;
+         insertionTime = 0;
+         countingTime = 0;
+         bubbleTime = 0;
+         random_quickTime = 0; 
+         java_quickTime = 0;
+
+         for(int i = 0; i<=iter; i++){
+            mergeTime += orden.mergeSort(arrayDecrescente.clone());
+            quickTime += orden.quickSort(arrayDecrescente.clone());
+            random_quickTime += orden.random_quickSort(arrayDecrescente.clone());
+            java_quickTime += orden.quickSort_Java(arrayDecrescente.clone());
+            selectionTime += orden.selectionSort(arrayDecrescente.clone());
+            countingTime += orden.countingSort(arrayDecrescente.clone());
+            insertionTime += orden.insertionSort(arrayDecrescente.clone());
+            bubbleTime += orden.bubbleSort(arrayDecrescente.clone());
+            
+            
+        }
+
+        System.out.printf("""
+            \n\nVetor em Ordem Descrescente:
+                bubbleSort time:      %d
+                insertionSort time:   %d
+                selectionSort time:   %d
+                mergeSort time:       %d
+                quickSort time:       %d
+                randomQuickSort time: %d
+                javaQuickSort time:   %d
+                countingSort time:    %d
+                """, bubbleTime, insertionTime, selectionTime, mergeTime, quickTime, random_quickTime, java_quickTime, countingTime);
+        
+         for(int i = 0; i<=iter; i++){
+            mergeTime += orden.mergeSort(arrayAleatorio.clone());
+            quickTime += orden.quickSort(arrayAleatorio.clone());
+            random_quickTime += orden.random_quickSort(arrayAleatorio.clone());
+            java_quickTime += orden.quickSort_Java(arrayAleatorio.clone());
+            selectionTime += orden.selectionSort(arrayAleatorio.clone());
+            countingTime += orden.countingSort(arrayAleatorio.clone());
+            insertionTime += orden.insertionSort(arrayAleatorio.clone());
+            bubbleTime += orden.bubbleSort(arrayAleatorio.clone());
+        }
+
+        System.out.printf("""
+            \n\nVetor aleatório:
+                bubbleSort time:      %d
+                insertionSort time:   %d
+                selectionSort time:   %d
+                mergeSort time:       %d
+                quickSort time:       %d
+                randomQuickSort time: %d
+                javaQuickSort time:   %d
+                countingSort time:    %d
+                """, bubbleTime, insertionTime, selectionTime, mergeTime, quickTime, random_quickTime, java_quickTime, countingTime);
+        
+    }
+
+    public static int[] cria_arrayCrecente(int tam){
+        int array[] = new int[tam];
+        for(int i = 0; i < array.length; i++){
+            array[i] = i;
+        }
+        return array;
+    }
+
+    public static int[] cria_arrayDecrescente(int tam){
+        int array[] = new int[tam];
+        for(int i = 0; i<tam; i++){
+            array[tam-i-1] = i;
+        }
+        return array;
+    }
+    
+    public static  int[] cria_arrayAleatorio(int tam){
+        int array[] = new int[tam];
+        Random rand = new Random();
+        for(int i = 0; i < tam; i++){
+            array[i] = rand.nextInt(5001);
+        }
+        return array;
+    }
+}
+
---
+++ src/atividade02/Ordenacao.java
@@ -0,0 +1,224 @@
+package atividade02;
+import java.util.Random;
+import java.util.Arrays;
+
+public class Ordenacao{
+    public boolean checaVetorOrdenado(int[] numeros){
+        for(int i =0;i<numeros.length;i++) {
+			if(numeros[i]>numeros[i+1]) {
+				return false;
+			}
+		}
+		return true;
+    }
+	
+	public long bubbleSort(int[] numeros){
+        return 1;
+    }
+	
+	public long selectionSort(int[] numeros){
+        return 1;
+    }
+	
+	public long insertionSort(int[] numeros){
+        long ini = System.nanoTime();
+        for (int i = 1; i < numeros.length; i++){
+            int key = numeros[i];
+            int j = i - 1;
+
+            while(j > 0 && numeros[j] > key){
+                numeros[j+1] = numeros[j];
+                j = j - 1;
+            }
+            numeros[j + 1] = key;
+        } 
+        return System.nanoTime() - ini;
+    }
+	
+	public long mergeSort(int[] numeros){
+        long antes= System.nanoTime();
+		long depois;
+		
+		int n = numeros.length;
+		
+		if(n<=1) {
+			depois = System.nanoTime();
+			return depois-antes;
+		}
+		
+		else {
+			auxMerge(numeros, 0, n-1);
+		}
+			depois = System.nanoTime();
+			return depois-antes;
+    }
+
+    private static void auxMerge(int[] numeros, int ini, int fim) {
+		if(ini<fim) {
+			int meio = (ini+fim)/2;
+			int[] resultante = new int[numeros.length];
+			
+			auxMerge(numeros, ini, meio);
+			auxMerge(numeros, meio + 1, fim);
+			
+			intercalar(numeros,resultante,ini,meio,fim);
+		}
+	}
+    
+    private static void intercalar(int[] numeros,int[] resultante, int ini, int meio, int fim) {
+		int esquerda = meio;
+		int direita = fim;
+		int i = ini;
+		int j = meio+1;
+		int index = ini;
+		
+		while(i <= esquerda && j <= direita) {
+			if(numeros[i] <= numeros[j]) {
+				resultante[index] = numeros[i];
+				i++;
+			}else {
+				resultante[index] = numeros[j];
+				j++;
+			}
+			index++;
+		}
+		
+		while (i<= meio) {
+			resultante[index] = numeros[i];
+			i++;
+			index++;
+		}
+		while (j<= fim) {
+			resultante[index] = numeros[j];
+			j++;
+			index++;
+		}
+		
+		
+		for(int k = ini;k<=fim;k++) {
+			numeros[k] = resultante[k];
+		}
+	}
+    //QUICKSORT
+    public long quickSort(int[] numeros){
+        long inicio = System.nanoTime();
+        this.quickSort_auxiliar(numeros, 0, numeros.length-1);
+        long fim = System.nanoTime();
+        return fim-inicio;
+    }
+
+    private void quickSort_auxiliar(int[] numeros, int left, int right){
+        if(left<right){
+            int pivot = this.quickSort_partition(numeros, left, right);
+            quickSort_auxiliar(numeros, left, pivot-1);
+            quickSort_auxiliar(numeros, pivot+1, right);
+        }
+    }
+
+    private int quickSort_partition(int[] numeros, int left, int right){
+        int pivot = numeros[left];
+        int i = left+1;
+        int j = right;
+        while(i <= j){
+            if(pivot>=numeros[i]){
+                i++;
+            }
+            else if(pivot<numeros[j]){
+                j--;
+            }
+            else{
+                swap(numeros, i, j);
+            }
+        }
+        swap(numeros, left, j);
+        return j;
+    }
+
+    static void swap(int[] numeros, int a, int b){
+        int aux = numeros[b];
+        numeros[b] = numeros[a];
+        numeros[a] = aux;
+    }
+
+	public long random_quickSort(int[] numeros){
+        long inicio = System.nanoTime();
+        this.random_quickSort_auxiliar(numeros, 0, numeros.length-1);
+        long fim = System.nanoTime();
+        return fim-inicio;
+    }
+
+    private void random_quickSort_auxiliar(int[] numeros, int left, int right){
+        if(left < right){
+            int pivot = random_quickSort_partition(numeros, left, right);
+            random_quickSort_auxiliar(numeros, left, pivot-1);
+            random_quickSort_auxiliar(numeros, pivot+1, right);
+        }
+    }
+
+    private int random_quickSort_partition(int[] numeros, int left, int right){
+        Random rand = new Random();
+        int pivotIndex = rand.nextInt(right-left)+left;
+        swap(numeros, pivotIndex, left);
+        int pivot = numeros[left];
+
+        int i = left+1;
+        int j = right;
+        while(i <= j){
+            if(pivot>=numeros[i]){
+                i++;
+            }
+            else if(pivot<numeros[j]){
+                j--;
+            }
+            else{
+                swap(numeros, i, j);
+            }
+        }
+        swap(numeros, left, j);
+        return j;
+    }
+
+    public long quickSort_Java(int[] numeros){
+        long ini = System.nanoTime();
+        Arrays.sort(numeros);
+        long fim = System.nanoTime();
+        return fim-ini;
+    }
+
+    public long countingSort(int[] numeros){
+        long ini = System.nanoTime();
+        int maxValue = this.maxValueOf(numeros) + 1;
+        int aux[] = new int[maxValue];
+        int arrayOrdenado[] = new int[numeros.length];
+        
+        for(int i = 0; i < maxValue; i++){
+            aux[i] = 0;
+        }
+        
+        for(int j = 0; j < numeros.length; j++){
+            aux[numeros[j]]++;
+        }
+
+        for(int i = 1; i < maxValue; i++){
+            aux[i] = aux[i] + aux[i-1];
+        }
+        
+        for(int i = numeros.length-1; i >= 0; i--){
+            arrayOrdenado[aux[numeros[i]] - 1] = numeros[i];
+            aux[numeros[i]]--;
+        }
+        long fim = System.nanoTime();
+        return fim-ini;
+    }
+
+    private int maxValueOf(int[] numeros){
+        int maior = numeros[0];
+        for(int i = 1; i < numeros.length; i++){
+            if(maior < numeros[i]){
+                maior = numeros[i];
+            }
+        }
+        return maior;
+    }
+}
+
